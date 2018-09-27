ae372e5c9df6afe96cb35975b0fc1c981b79fdb0
Eleonore Carpentier <shaeli@users.noreply.github.com>
2018-09-27 19:16:00 +0200

    Adding decoder

--- python/coder.py
+++ python/coder.py
@@ -26,3 +26,16 @@ while (i % 3 != 0):
 # Printing color array :
 for i in range(0, len(payloadByteArray)):
     print "{}: {} {} {}".format(i, payloadByteArray[i][0], payloadByteArray[i][1],payloadByteArray[i][2])
+
+##Decoder##
+mask = 255 - mask
+bin_text = ""
+for i in range(0, len(payloadByteArray)):
+    for j in range(3):
+        current_bin = "{0:b}".format((mask & payloadByteArray[i][j]))
+        while len(current_bin) != 4:
+            current_bin = "0" + current_bin
+        bin_text = bin_text + current_bin
+result = "".join(chr(int(bin_text[i:i+8], 2)) for i in xrange(0, len(bin_text),8))
+
+print result
