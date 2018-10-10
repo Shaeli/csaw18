24d77aac2a3aea03998143e81da40795d00b301b
Corentin Thomasset <33403840+corentinthomasset@users.noreply.github.com>
2018-10-10 17:09:29 -0400

    Creation of extractByteArrayFromPcap method

--- python/pcap.py
+++ python/pcap.py
@@ -1,18 +1,18 @@
 import re
 from scapy.all import *
 
-packets = rdpcap('white.pcap')
+def extractByteArrayFromPcap(pcap):
+    packets = rdpcap(pcap)
+    byteArray = []
+    for pkt in packets:
+        if pkt.haslayer('Write Request'):
+            pkt.show()
+            color = re.search(b'\x56(.)(.)(.)\x00\xf0\xaa', pkt['Write Request'].data)
+            byteArray.append([
+                int.from_bytes(color.group(1), 'big'),
+                int.from_bytes(color.group(2), 'big'),
+                int.from_bytes(color.group(3), 'big')])
 
-payloadByteArray = []
+    return byteArray
 
-for pkt in packets:
-    if pkt.haslayer('Write Request'):
-        pkt.show()
-        color = re.search(b'\x56(.)(.)(.)\x00\xf0\xaa', pkt['Write Request'].data)
-        payloadByteArray.append([
-            int.from_bytes(color.group(1), 'big'),
-            int.from_bytes(color.group(2), 'big'),
-            int.from_bytes(color.group(3), 'big')])
-
-for i in range(0, len(payloadByteArray)):
-    print("{}: {} {} {}".format(i, payloadByteArray[i][0], payloadByteArray[i][1],payloadByteArray[i][2]))
+print(extractByteArrayFromPcap('white.pcap'))
