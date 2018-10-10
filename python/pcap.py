40c3357c19216f4dd5fe16e2a74b8fe3ca4de39f
Corentin Thomasset <33403840+corentinthomasset@users.noreply.github.com>
2018-10-10 17:05:34 -0400

    Edit byteArray Output to be compatible with coder methods

--- python/pcap.py
+++ python/pcap.py
@@ -1,7 +1,7 @@
 import re
 from scapy.all import *
 
-packets = rdpcap('lol.pcapng')
+packets = rdpcap('white.pcap')
 
 payloadByteArray = []
 
@@ -9,7 +9,10 @@ for pkt in packets:
     if pkt.haslayer('Write Request'):
         pkt.show()
         color = re.search(b'\x56(.)(.)(.)\x00\xf0\xaa', pkt['Write Request'].data)
-        payloadByteArray.append([color.group(1),color.group(2),color.group(3)])
+        payloadByteArray.append([
+            int.from_bytes(color.group(1), 'big'),
+            int.from_bytes(color.group(2), 'big'),
+            int.from_bytes(color.group(3), 'big')])
 
 for i in range(0, len(payloadByteArray)):
     print("{}: {} {} {}".format(i, payloadByteArray[i][0], payloadByteArray[i][1],payloadByteArray[i][2]))
