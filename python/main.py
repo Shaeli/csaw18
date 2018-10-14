ce8bf6ee55d89c66bb0af2280aaca10ca463ea78
Shaeli <12385201+Shaeli@users.noreply.github.com>
2018-10-14 14:09:41 +0200

    Cleaning

--- python/main.py
+++
@@ -1,9 +0,0 @@
-from coder import *
-from pcap import *
-
-def exctractPayloadFromPcap(pcap, nbBits):
-    colorsByteArray = extractByteArrayFromPcap(pcap)
-    payload = decode(colorsByteArray, nbBits)
-    return payload
-
-print(exctractPayloadFromPcap('white.pcap', 4))
